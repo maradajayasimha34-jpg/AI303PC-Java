3abfd4b7a6d96438f79d88256c19d0c892499518
maradajayasimha34-jpg <maradajayasimha34@gmail.com>
2026-07-07 11:42:00 +0530

    Add files via upload

---
+++ SecondJava.java
@@ -0,0 +1,8 @@
+class SecondJava {
+    static {
+        System.out.println("I am a static block");
+    }
+    public static void main(String... args) {
+        System.out.println("I am main block");
+    }
+}
